ab5941c18ba5467bff75f55d66ccba54db729673
0429oonishi <oonishireon110@gmail.com>
2021-07-15 18:37:30 +0900

    完成

--- oonishiReonKadai10/ViewController.swift
+++ oonishiReonKadai10/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private let prefectureNames = Prefecture.name
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupTableView()
+        
     }
-
-
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(PrefectureTableViewCell.nib,
+                           forCellReuseIdentifier: PrefectureTableViewCell.identifier)
+        tableView.tableFooterView = UIView()
+    }
+    
+    
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return prefectureNames.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: PrefectureTableViewCell.identifier
+        ) as! PrefectureTableViewCell
+        let prefecutreName = prefectureNames[indexPath.row]
+        cell.tag = indexPath.row + 1
+        cell.configure(with: prefecutreName)
+        return cell
+    }
+    
+    
+}
