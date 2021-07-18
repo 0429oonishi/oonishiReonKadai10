7bd2fb47d515125c350d87dbda1f18e3814b96a4
0429oonishi <oonishireon110@gmail.com>
2021-07-18 17:30:59 +0900

    PrefectureVCリファクタリング

--- oonishiReonKadai10/Views/Controllers/PrefectureViewController.swift
+++ oonishiReonKadai10/Views/Controllers/PrefectureViewController.swift
@@ -15,7 +15,7 @@ final class PrefectureViewController: UIViewController {
     
     private let viewModel: PrefectureViewModelType = PrefectureViewModel()
     private let disposeBag = DisposeBag()
-    private var prefectureNames: Observable<[String]> {
+    private var prefectureNames: [String] {
         viewModel.outputs.prefectureNames
     }
     
@@ -23,26 +23,39 @@ final class PrefectureViewController: UIViewController {
         super.viewDidLoad()
         
         setupTableView()
-        setupBindings()
         
     }
     
     private func setupTableView() {
         tableView.register(PrefectureTableViewCell.nib,
                            forCellReuseIdentifier: PrefectureTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
         tableView.tableFooterView = UIView()
     }
     
-    private func setupBindings() {
-        // MARK: - ToDo データソースを使う
-        prefectureNames.bind(
-            to: tableView.rx.items(cellIdentifier: PrefectureTableViewCell.identifier,
-                                   cellType: PrefectureTableViewCell.self)
-        ) { row, element, cell in
-            cell.tag = row + 1
-            cell.configure(with: element)
-        }
-        .disposed(by: disposeBag)
+}
+
+extension PrefectureViewController: UITableViewDelegate {
+    
+}
+
+extension PrefectureViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return prefectureNames.count
     }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: PrefectureTableViewCell.identifier
+        ) as! PrefectureTableViewCell
+        let prefectureName = prefectureNames[indexPath.row]
+        cell.configure(prefectureName: prefectureName, index: indexPath.row)
+        return cell
+    }
+    
     
 }
